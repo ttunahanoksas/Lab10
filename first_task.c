5c930996593c35cd51fce1983ca479e8a53c4419
ttoksas <58305934+ttunahanoksas@users.noreply.github.com>
2025-11-20 10:39:17 +0100

    Update first_task.c

--- first_task.c
+++ first_task.c
@@ -1,30 +1,33 @@
-/* Task description:
-
-
-We can get any element of an arithmetic sequence (arithmetic progression) by adding the common difference to the previous element:
-
-an = an-1 + d
-
-Considering the sequence as a function, realize a recursive function than returns the nth element of the sequence! The parameters of the function are: arithmetig_seq(n, start, diff), the sequence number of the required element, the starting value and the difference. What shall be the base case for this recursion?
-
-Realize a similar (recursive) function that returns the nth element of a geometric sequence. The definition is very similar to AP but a quotient is given and multiple instaed of add:
-
-bn = bn-1 · q
-
-Complete the program to print the first 10 elements of the sequences! For arithmetic, the base case is 1 and it is increasing by 5, for geometric sequence the first element is 2 and the multiplication factor is 2.
-
-    1     6     11    16    21    26    31    36    41    46 
-    2     4     8     16    32    64   128   256   512  1024 
-
-
-
-*/
-
-
 #include <stdio.h>
 
-int main(){
+int arithmetic_seq(int n, int start, int diff) {
+    if (n == 1) {
+        return start;
+    } else {
+        return arithmetic_seq(n - 1, start, diff) + diff;
+    }
+}
 
+int geometric_seq(int n, int start, int q) {
+    if (n == 1) {
+        return start;
+    } else {
+        return geometric_seq(n - 1, start, q) * q;
+    }
+}
 
-  return 0;
+int main() {
+ 
+    printf("Arithmetic Progression:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", arithmetic_seq(i, 1, 5));  // Start = 1, diff = 5
+    }
+    printf("\n");
+    printf("Geometric Progression:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", geometric_seq(i, 2, 2));  // Start = 2, q = 2
+    }
+    printf("\n");
+
+    return 0;
 }
